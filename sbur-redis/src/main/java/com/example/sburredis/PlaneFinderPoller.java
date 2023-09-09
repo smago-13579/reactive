df6a758e2b0f4eb993b5d0aa06eea8f62c7bdfa9
smago-13579 <smago@student.21-school.ru>
2023-09-09 20:23:16 +0600

    1

---
+++ sbur-redis/src/main/java/com/example/sburredis/PlaneFinderPoller.java
@@ -0,0 +1,38 @@
+package com.example.sburredis;
+
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisOperations;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@EnableScheduling
+@Component
+public class PlaneFinderPoller {
+    private WebClient client = WebClient.create("http://localhost:7634/aircraft");
+    private final RedisOperations<String, AirCraft> redisOperations;
+    private final RedisConnectionFactory factory;
+
+    public PlaneFinderPoller(RedisOperations<String, AirCraft> redisOperations, RedisConnectionFactory factory) {
+        this.redisOperations = redisOperations;
+        this.factory = factory;
+    }
+
+    @Scheduled(fixedRate = 1000)
+    private void pollPlanes() {
+        factory.getConnection().serverCommands().flushDb();
+
+        client.get()
+                .retrieve()
+                .bodyToFlux(AirCraft.class)
+                .filter(airCraft -> !airCraft.getReg().isEmpty())
+                .toStream()
+                .forEach(airCraft -> redisOperations.opsForValue().set(airCraft.getReg(), airCraft));
+
+        redisOperations.opsForValue()
+                .getOperations()
+                .keys("*")
+                .forEach(airCraftKey -> System.out.println(redisOperations.opsForValue().get(airCraftKey)));
+    }
+}
