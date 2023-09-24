aba7b33f2ad40ab9b65e5579f8cfe0a9dc0421ac
smago <smago@student.21-school.ru>
2023-09-24 10:15:35 +0600

    mongo

--- sbur-redis/src/test/java/com/example/sburredis/SburRedisApplicationTests.java
+++ sbur-redis/src/test/java/com/example/sburredis/SburRedisApplicationTests.java
@@ -3,11 +3,15 @@ package com.example.sburredis;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+//@SpringBootTest
 class SburRedisApplicationTests {
 
     @Test
     void contextLoads() {
+        System.out.println(LocalDateTime.now().toLocalDate().compareTo(LocalDate.now().minusDays(1)));
     }
 
 }
