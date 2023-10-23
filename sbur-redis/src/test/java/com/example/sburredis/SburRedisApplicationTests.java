9dd3cb7b90413d47a7f9adbf4f6569fc65c04e95
smago <smago@student.21-school.ru>
2023-10-24 04:57:25 +0600

    new file

--- sbur-redis/src/test/java/com/example/sburredis/SburRedisApplicationTests.java
+++ sbur-redis/src/test/java/com/example/sburredis/SburRedisApplicationTests.java
@@ -5,6 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 
 //@SpringBootTest
 class SburRedisApplicationTests {
@@ -13,5 +14,16 @@ class SburRedisApplicationTests {
     void contextLoads() {
         System.out.println(LocalDateTime.now().toLocalDate().compareTo(LocalDate.now().minusDays(1)));
     }
+
+    @Test
+    void someTest() {
+        List<Order> orders = List.of(new Order(1L), new Order(2L), new Order(3L));
+
+        Order sorted = orders.stream().min((a1, a2) -> Math.toIntExact(a1.id() - a2.id())).get();
+
+        System.out.println(sorted);
+    }
+
+    record Order(Long id) {}
 
 }
