473f9089eabd4921453c9d601e37013ced034bb5
Alibekov Sultan <smago-13579@oma.kz>
2024-12-06 18:37:21 +0500

    StringTest has been updated

--- demo/src/test/java/com/example/leetcode/easy/StringTest.java
+++ demo/src/test/java/com/example/leetcode/easy/StringTest.java
@@ -42,4 +42,61 @@ public class StringTest {
         }
         return res;
     }
+
+    @Test
+    void arraysTest() {
+        int[] chars = new int[10];
+        int[] chars2 = new int[11];
+        int[] chars3 = new int[10];
+        chars3[0] = 'a';
+
+        System.out.println(chars.equals(chars));
+        System.out.println(chars2.equals(chars));
+        System.out.println(chars3.equals(chars));
+        System.out.println(Arrays.equals(chars, chars2));
+        System.out.println(Arrays.equals(chars, chars3));
+
+    }
+
+    @Test
+    void atoiTest() {
+        int res = atoi("-21474848");
+        System.out.println(res);
+    }
+
+    private int atoi(String s) {
+        int i = 0;
+        String sign = "";
+
+        while (i < s.length()) {
+            if (s.charAt(i) == ' ') {
+                i++;
+            } else if (s.charAt(i) == '-' || s.charAt(i) == '+') {
+                sign += s.charAt(i);
+                i++;
+            } else {
+                break;
+            }
+        }
+
+        if (sign.length() > 1 || i == s.length()) {
+            return 0;
+        }
+        boolean negative = sign.equals("-");
+        int j = i;
+        int result = 0;
+
+        while (j < s.length() && s.charAt(j) >= '0' && s.charAt(j) <= '9') {
+            if (!negative && (Integer.MAX_VALUE - (s.charAt(j) - '0')) / 10 < result) {
+                return Integer.MAX_VALUE;
+            }
+
+            if (negative && (Integer.MIN_VALUE + (s.charAt(j) - '0')) / 10 + result > 0) {
+                return Integer.MIN_VALUE;
+            }
+            result = result * 10 + (s.charAt(j) - '0');
+            j++;
+        }
+        return negative ? -result : result;
+    }
 }
